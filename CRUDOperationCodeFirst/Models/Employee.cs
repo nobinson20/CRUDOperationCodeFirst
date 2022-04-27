684ec37edd8464043db58d32182161e14bfe40ff
nobinson20 <nobinson20@gmail.com>
2022-04-28 01:54:20 +0900

    Application is fully working and stable

---
+++ CRUDOperationCodeFirst/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace CRUDOperationCodeFirst.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? JoiningDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
